360b18682382e79da0a8c8affac922b2fb7c0c3c
Kaan Latinler <kaanlatinlerhd@gmail.com>
2024-08-25 04:19:36 +0300

    id added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/getAllUsers', userController.getAllUsers);
-router.get('/getUserById', userController.getUserById);
+router.get('/getUserById/:id', userController.getUserById);
 router.post('/createUser', userController.createUser);
-router.put('/updateUser', userController.updateUser);
-router.delete('/deleteUser', userController.deleteUser);
+router.put('/updateUser/:id', userController.updateUser);
+router.delete('/deleteUser/:id', userController.deleteUser);
 
 module.exports = router;
